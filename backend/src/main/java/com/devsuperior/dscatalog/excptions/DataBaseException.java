45a4d665f027c17e31c3ffa7d816e76b0341a485
Pedroigoor <pedro.igor10@live.com>
2021-11-09 14:45:30 -0300

    remoção controle advice do DataBaseException

--- backend/src/main/java/com/devsuperior/dscatalog/excptions/DataBaseException.java
+++ backend/src/main/java/com/devsuperior/dscatalog/excptions/DataBaseException.java
@@ -1,9 +1,10 @@
 package com.devsuperior.dscatalog.excptions;
 
-import org.springframework.web.bind.annotation.ControllerAdvice;
-
-@ControllerAdvice
 public class DataBaseException extends RuntimeException {
+
+	/**
+	 * 
+	 */
 	private static final long serialVersionUID = 1L;
 
 	public DataBaseException(String msg) {
