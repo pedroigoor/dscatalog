aa5b503494cd8f8a08772016998d244a06acc160
pedro.Igor <pedro.igor10@live.com>
2022-05-23 17:42:14 -0300

    spyOn e mock do getToken

---
+++ frontweb/src/util/__tests__/auth.spec.ts
@@ -0,0 +1,24 @@
+import { hasAnyRoles } from "util/auth";
+import * as TokenModule from "../token";
+
+
+describe('HasAnyRoles tests ', () => {
+ 
+    test('should return true when emputy list', () => {
+        const result = hasAnyRoles([])       
+        expect(result).toEqual(true);
+      });
+
+      
+    test('should return true when user has given role', () => {
+        jest.spyOn(TokenModule,'getTokenData').mockReturnValue({
+        exp: 0,
+        user_name: 'string',
+        authorities:['ROLE_ADMIN',  "ROLE_OPERATOR"]
+        })
+        const result = hasAnyRoles(['ROLE_ADMIN'])       
+        expect(result).toEqual(true);
+      });
+  });
+  
+  
